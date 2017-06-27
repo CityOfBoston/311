68add9d76af65f0d32589ddc427264f283d5204f
Fin Hopkins <fin.hopkins@boston.gov>
2017-06-27 10:29:59 -0400

    Switch to Prettier / common ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "jest": true,
+    "es6": true,
+    "node": true
+  },
+  "plugins": [
+    "flowtype",
+    "import",
+    "react",
+    "prettier",
+    "jsx-a11y"
+  ],
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:flowtype/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+    "prettier/flowtype",
+    "prettier/react"
+  ],
+  "rules": {
+    // Trips up with next/link, which adds the href automatically
+    "jsx-a11y/anchor-is-valid": 0,
+    // Seems broken in v6.0.0
+    "jsx-a11y/label-has-for": 0,
+    "prettier/prettier": ["error", {
+      "singleQuote": true,
+      "trailingComma": "all",
+      "jsxBracketSameLine": true
+    }]
+  }
+};
